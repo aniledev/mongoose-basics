ccec4cc86243d647b344b58b6695664f874e421a
aniledev <elinamcgill@gmail.com>
2021-06-04 23:08:40 -0400

    REFRACTOR: refractor connection/error using promise chaining

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/movieApp", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose
+  .connect("mongodb://localhost:27017/movieApp", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connection open!"))
+  .catch((err) => {
+    console.log("Oh no, error!");
+    console.log(err);
+  });
 
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", function () {
-  // we're connected!
-  console.log("Connection open!");
-});
+// const db = mongoose.connection;
+// db.on("error", console.error.bind(console, "connection error:"));
+// db.once("open", function () {
+//   // we're connected!
+//   console.log("Connection open!");
+// });
