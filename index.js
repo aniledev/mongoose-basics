b25fc8bc91bc61d5180878f11e9686a5c7a89f39
aniledev <elinamcgill@gmail.com>
2021-06-04 23:37:14 -0400

    FEAT: use mongoose insertMany() to create some dummy entries in the database

--- index.js
+++ index.js
@@ -33,3 +33,19 @@ CREATE A MODEL CLASS
 4. new instances of the model class can be created and saved to the database 
 */
 const Movie = mongoose.model("Movie", movieSchema);
+const amadeus = new Movie({
+  title: "Amadeus",
+  year: 1986,
+  score: 9.2,
+  rating: "R",
+});
+
+// amadeus.save() is what transfers the javascript object to the databse
+
+Movie.insertMany([
+  { title: "Amelie", year: 2001, score: 8.3, rating: "R" },
+  { title: "Alien", year: 1979, score: 8.1, rating: "R" },
+  { title: "The Iron Giant", year: 1999, score: 7.5, rating: "PG" },
+  { title: "Stand By Me", year: 1986, score: 8.6, rating: "R" },
+  { title: "Moonrise Kingdom", year: 2012, score: 7.3, rating: "PG-13" },
+]);
