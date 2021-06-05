f10db704664eb23f05017a614978983931be2800
aniledev <elinamcgill@gmail.com>
2021-06-04 23:02:47 -0400

    FEAT: create logs for connection open and connection error

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ mongoose.connect("mongodb://localhost:27017/movieApp", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+  // we're connected!
+  console.log("Connection open!");
+});
